33726e9cc5381aca6b56784ab74106f6c64b018a
Eric Koleda <koleda.eric@gmail.com>
2022-04-14 10:27:17 -0400

    Make embed dynamic

--- code-blocks/embed/functions/index.js
+++ code-blocks/embed/functions/index.js
@@ -1,25 +1,27 @@
+const fs = require("fs");
+const path = require('path');
+
 const functions = require("firebase-functions");
 
 exports.index = functions.https.onRequest((request, response) => {
-  let url = `https://${request.headers.host}${request.url}`;
-  response.send(`
-    <html>
-      <head>
-        <link rel="alternate" 
-              type="application/json+oembed"
-              href="/oembed?url=${encodeURIComponent(url)}&format=json"
-              title="Code Block Embed" />
-      </head>
-      <body>Loading...</body>
-    </html>
-  `);
+  let host = request.headers["x-forwarded-host"] || request.headers.host;
+  let url = new URL(`${request.protocol}://${host}${request.path}`);
+  for (let key of Object.keys(request.query)) {
+    url.searchParams.append(key, request.query[key]);  
+  }
+  let template = fs.readFileSync(path.resolve("index.html")).toString();
+  let page = template
+    .replace("{protocol}", request.protocol)
+    .replace("{host}", host)
+    .replace("{url}", encodeURIComponent(url));
+  response.send(page);
 });
 
 exports.oembed = functions.https.onRequest((request, response) => {
   response.send({
     type: "rich",
     version: "1.0",
-    html: "This is my embed",
+    html: `<iframe src="${request.query.url}">`,
     width: 400,
     height: 200,
   });
