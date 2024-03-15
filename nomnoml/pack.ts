22a2b7be614089b9f895a1d77430be9b54df9d76
Eric Koleda <koleda.eric@gmail.com>
2024-03-15 17:50:30 -0400

    Add ability to generate markup from a table.

--- nomnoml/pack.ts
+++ nomnoml/pack.ts
@@ -31,3 +31,40 @@ pack.addColumnFormat({
   instructions: "Enter nomnoml markup and it will be rendered as a diagram.",
   formulaName: "Diagram",
 });
+
+pack.addFormula({
+  name: "GenerateMarkup",
+  description: "Generate nomnoml markup from a table that has a parent-child relationship.",
+  parameters: [
+    coda.makeParameter({
+      type: coda.ParameterType.SparseStringArray,
+      name: "labelColumn",
+      description: "The entire column of the table that contains the label for each row.",
+    }),
+    coda.makeParameter({
+      type: coda.ParameterType.SparseStringArray,
+      name: "parentColumn",
+      description: "The entire column of the table that contains the parent for each row, if any.",
+    }),
+  ],
+  resultType: coda.ValueType.String,
+  cacheTtlSecs: OneDaySecs,
+  execute: async function (args, context) {
+    let [labels, parents] = args;
+    [labels, parents].reduce((result, list) => {
+      if (!result) return list.length;
+      if (result != list.length) throw new coda.UserVisibleError("All lists must be the same length.");
+      return result;
+    }, 0);
+    let lines = labels.map((label, i) => {
+      let parent = parents[i];
+      if (!label) return "";
+      if (parent) {
+        return `[${parent}] -> [${label}]`;
+      } else {
+        return `[${label}]`;
+      }
+    });
+    return lines.filter(Boolean).join("\n");
+  },
+});
