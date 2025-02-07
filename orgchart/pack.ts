0129726d6fcd7567207af3e56e05c96aabd731f1
Eric Koleda <koleda.eric@gmail.com>
2025-02-07 10:39:15 -0500

    Improvements

--- orgchart/pack.ts
+++ orgchart/pack.ts
@@ -10,28 +10,34 @@ export const pack = coda.newPack();
 
 pack.addFormula({
   name: "OrgChart",
-  description: "Draws an org chart using a table of people.",
+  description: "Draws an org chart from a table of people.",
   parameters: [
     coda.makeParameter({
       type: coda.ParameterType.SparseStringArray,
       name: "nameColumn",
-      description: "The column containing the name of the person.",
+      description: "The column containing the name of each person. Ex: EmployeesTable.Name",
     }),
     coda.makeParameter({
       type: coda.ParameterType.SparseStringArray,
       name: "reportsToColumn",
-      description: "The column containing the name of the person they report to.",
+      description: "The column containing the name of the person they report to. Ex: EmployeesTable.Manager",
     }),
     coda.makeParameter({
       type: coda.ParameterType.SparseStringArray,
       name: "descriptionColumn",
-      description: "Optionally, the column containing a description (title, department, location, etc) of the person.",
+      description: "Optionally, the column containing a description (title, department, location, etc) of the person. Ex: EmployeesTable.Title",
       optional: true,
     }),
     coda.makeParameter({
       type: coda.ParameterType.String,
-      name: "color",
-      description: "If specified, which CSS color value to use for the background of the nodes.",
+      name: "backgroundColor",
+      description: "If specified, which CSS color value to use for the background of the nodes. Ex: #EE5A29.",
+      optional: true,
+    }),
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: "textColor",
+      description: "If specified, which CSS color value to use for the text in the nodes. Ex: white",
       optional: true,
     }),
   ],
@@ -47,7 +53,8 @@ pack.addFormula({
       names, 
       managers, 
       descriptions = new Array(names.length), 
-      color,
+      backgroundColor,
+      textColor,
     ] = args;
     [names, descriptions, managers].reduce((result, list) => {
       if (!result) return list.length;
@@ -64,7 +71,8 @@ pack.addFormula({
     let input = LZString.compressToEncodedURIComponent(csv);
     let url = coda.withQueryParams(BaseUrl, {
       i: input,
-      c: color,
+      b: backgroundColor,
+      t: textColor,
     });
     if (url.length > MaxUrlLength) {
       throw new coda.UserVisibleError("Org chart too large. Try removing data if possible.");
